95761893161fe0042aafc30f2250e9d89ce96fbd
Camila Correia <96155623+ItsCamyx@users.noreply.github.com>
2023-06-25 23:16:43 -0300

    feat: add first call in frontend (#11)

--- dialog-challlenge-front/src/pages/mainPage/mainPage.tsx
+++ dialog-challlenge-front/src/pages/mainPage/mainPage.tsx
@@ -1,9 +1,13 @@
 import { Card } from '../../components/card/card';
 import { Header } from '../../components/header/header';
 import { users } from '../../mock/userMock';
+import { LIST_QUERY } from '../../services/listQuery';
+import { useQuery } from '@apollo/client';
 import { ContainerCard, ContainerSearch, MainContainer } from './mainPageStyle';
+import { IPerson } from '../../interface/user';
 
 export const MainPage = () => {
+  const { data } = useQuery(LIST_QUERY);
   return (
     <>
       <MainContainer>
@@ -11,7 +15,7 @@ export const MainPage = () => {
           <Header />
         </ContainerSearch>
         <ContainerCard>
-          {users.map((user) => {
+          {data.list.map((user: IPerson) => {
             return <Card key={user._id} user={user} />;
           })}
         </ContainerCard>
