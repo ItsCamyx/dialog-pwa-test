143f84fd969e2ebc4e1e6d2a25c3ab9643d74479
ItsCamyx <96155623+ItsCamyx@users.noreply.github.com>
2023-06-28 00:27:45 -0300

    fix: adjust validation

--- dialog-challlenge-front/src/components/header/header.tsx
+++ dialog-challlenge-front/src/components/header/header.tsx
@@ -1,13 +1,49 @@
-import {
-  SearchInput,
-  Title,
-} from './headerStyle';
+import { useContext, useEffect } from 'react';
+import { useApolloClient } from '@apollo/client';
+import { SearchInput, Title } from './headerStyle';
+import SearchContext from '../../contexts/searchContext';
+import { LIST_QUERY } from '../../services/listQuery';
+import { toast } from 'react-toastify';
+import { UserContext } from '../../contexts/userContext';
 
-export const Header = () => {
+interface IHeader {
+  isHidden?: boolean;
+}
+export const Header = ({ isHidden }: IHeader) => {
+  const apolloClient = useApolloClient();
+  const { search, setSearch, setLoading } = useContext(SearchContext);
+  const { setUsers } = useContext(UserContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await apolloClient.query({
+          query: LIST_QUERY,
+          variables: { name: search },
+        });
+        setUsers(response?.data?.list);
+        setLoading(false);
+      } catch (error) {
+        toast.error('Erro ao carregar');
+      }
+    };
+
+    fetchData();
+  }, [apolloClient, setUsers, search, setLoading]);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <>
-          <Title>MySocial</Title>
-          <SearchInput type='text' placeholder='Procure por alguém' />
+      <Title>MySocial</Title>
+      <SearchInput
+        type='text'
+        placeholder='Procure por alguém'
+        onChange={handleChange}
+        hidden={isHidden}
+      />
     </>
   );
 };
--- dialog-challlenge-front/src/components/userAvatar/userAvatar.tsx
+++ dialog-challlenge-front/src/components/userAvatar/userAvatar.tsx
@@ -10,29 +10,33 @@ import {
 } from './userAvatarStyle';
 
 interface IAvatarProps {
-  selectUser: IPerson | IFriend;
+  selectUser: IPerson | IFriend | undefined;
 }
 export const UserAvatar = ({ selectUser }: IAvatarProps) => {
   return (
     <Container>
-      <TitleAvatarChoose>Usuário selecionado</TitleAvatarChoose>
-      <ContainerAvatar>
-        <div>
-          <Avatar src={selectUser.picture} />
-        </div>
-        <AvatarContainerDetails>
-          <div>
-            <AvatarSubTitle>Nome</AvatarSubTitle>
-            <AvatarSubTitle>Idade</AvatarSubTitle>
-            <AvatarSubTitle>Email</AvatarSubTitle>
-          </div>
-          <div>
-            <AvatarSubTitleProps>{selectUser.name}</AvatarSubTitleProps>
-            <AvatarSubTitleProps>{selectUser.age}</AvatarSubTitleProps>
-            <AvatarSubTitleProps>{selectUser.email}</AvatarSubTitleProps>
-          </div>
-        </AvatarContainerDetails>
-      </ContainerAvatar>
+      {selectUser ? (
+        <>
+          <TitleAvatarChoose>Usuário selecionado</TitleAvatarChoose>
+          <ContainerAvatar>
+            <div>
+              <Avatar src={selectUser.picture} />
+            </div>
+            <AvatarContainerDetails>
+              <div>
+                <AvatarSubTitle>Nome</AvatarSubTitle>
+                <AvatarSubTitle>Idade</AvatarSubTitle>
+                <AvatarSubTitle>Email</AvatarSubTitle>
+              </div>
+              <div>
+                <AvatarSubTitleProps>{selectUser.name}</AvatarSubTitleProps>
+                <AvatarSubTitleProps>{selectUser.age}</AvatarSubTitleProps>
+                <AvatarSubTitleProps>{selectUser.email}</AvatarSubTitleProps>
+              </div>
+            </AvatarContainerDetails>
+          </ContainerAvatar>
+        </>
+      ) : null}
     </Container>
   );
 };
